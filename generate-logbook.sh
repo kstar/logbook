d0278cf1c88a95a0de76e8cdd362925abcd3f545
Akarsh Simha <akarsh.simha@kdemail.net>
2013-01-27 03:35:18 -0600

    First version for testing

--- generate-logbook.sh
+++ generate-logbook.sh
@@ -1,8 +1,86 @@
-dest_dir='/tmp'
-list=''
-for messier_number in `seq 1 110`; do
-    object_underscored=`echo "M_${messier_number}"`
-    object=`echo $object_underscored | sed 's/_/ /g'`
+#!/bin/bash
+
+#
+# This script is Copyright (c) 2013 Akarsh Simha <akarsh.simha@kdemail.net> 
+#
+# This script is licensed under the GNU General Public License v2 or
+# any later version at your convenience.
+#
+# Please see the LICENSE file for a copy of the GNU General Public
+# License
+#
+
+#
+# Generate a logbook for a given list of celestial objects.
+#
+# This script uses KStars and LaTeX to generate observing log books
+# for a list of celestial objects.
+#
+# The objects must exist in KStars. KStars must be running and be
+# accessible via qdbus.
+#
+# Dependencies: xmlstarlet, inkscape, pdflatex, kstars, ImageMagick (convert), pdftk
+#
+
+# Invokation: ./generate-logbook.sh <project name> [object list file] ["build" directory]
+
+DEBUG="1"
+
+if [ -z $1 ]; then
+
+    echo "ERROR: No project name supplied. The project name is the title of the logbook."
+    echo "Invokation: ./generate-logbook.sh <project name> [object list file] [\"build\" directory]"
+    exit 1;
+fi;
+
+if [ -z $2 ]; then
+    OBJECT_LIST='objectlist.txt'
+else
+    OBJECT_LIST=$2
+fi;
+
+if [ $DEBUG ]; then echo $OBJECT_LIST; fi;
+
+if [ -z $3 ]; then
+    BUILD_DIR='build'
+else
+    BUILD_DIR=$3
+fi;
+
+if [ ! -f $OBJECT_LIST ]; then
+    echo "Object list file " $OBJECT_LIST " not found."
+    echo "Invokation: ./generate-logbook.sh <project name> [object list file] [\"build\" directory]"
+    exit 1;
+fi;
+
+mkdir -p $BUILD_DIR; # Create the "build" directory
+
+list='' # List of objects for final processing
+
+echo "" > ${BUILD_DIR}/Objects.tex
+echo "" > ${BUILD_DIR}/ConstType.txt
+echo "" > ${BUILD_DIR}/ObjectsByType.tex
+echo "" > ${BUILD_DIR}/ObjectsByConstellation.tex
+while read object_list_line; do
+
+    if [ $DEBUG ]; then echo "Processing line: " $object_list_line; fi;
+
+    object=`echo $object_list_line | awk -F'|' '{ print $1 }'` # Retrieve the name of the object from the list
+    object_description=`echo $object_list_line | awk -F'|' '{ print $2 }'` # Retrieve a description of the object
+    object_observability=`echo $object_list_line | awk -F'|' '{ print $3 }'` # Retrieve an observability string indicating whether the object is city / binocular observable
+
+    if [ -n $object_description ]; then
+	object_description_with_prefix="\textbf{Description:} " $object_description;
+    else
+	object_description_with_prefix=""
+    fi;
+
+    if [ $DEBUG ]; then echo "Object: " $object; fi;
+
+    object_underscored=`echo $object | sed 's/ /_/g'`; # An underscored copy of the object for use in file names etc
+
+    if [ $DEBUG ]; then echo "Underscored: " $object_underscored; fi;
+
     XML=`qdbus org.kde.kstars /KStars org.kde.kstars.getObjectDataXML "$object"`
     maj_axis=`echo $XML | xmlstarlet sel -t -m "object" -v "Major_Axis"`
     min_axis=`echo $XML | xmlstarlet sel -t -m "object" -v "Minor_Axis"`
@@ -47,48 +125,34 @@ for messier_number in `seq 1 110`; do
     height=`qdbus org.kde.kstars /KStars org.kde.kstars.getSkyMapDimensions | sed 's,^.*x,,'`
     FOV=`echo "20*${skymap_aspect_ratio}" | bc -l`
 
+    if [ $DEBUG ]; then echo "Capturing sky map with FOV = " $FOV; fi;
+
     # Export all three sky images
     qdbus org.kde.kstars /KStars org.kde.kstars.lookTowards "$object"
-    qdbus org.kde.kstars /KStars org.kde.kstars.setApproxFOV $FOV
-    qdbus org.kde.kstars /KStars org.kde.kstars.exportImage ${dest_dir}/${object_underscored}_skychart.svg
+    qdbus org.kde.kstars /KStars org.kde.kstars.setApproxFOV ${FOV}
+#    echo "qdbus org.kde.kstars /KStars org.kde.kstars.exportImage `pwd`/${BUILD_DIR}/${object_underscored}_skychart.svg"
+    qdbus org.kde.kstars /KStars org.kde.kstars.exportImage `pwd`/${BUILD_DIR}/${object_underscored}_skychart.svg
     
-    skychart_PDF=${dest_dir}/${object_underscored}_skychart.pdf
-    inkscape -T -A $skychart_PDF ${dest_dir}/${object_underscored}_skychart.svg
+    skychart_PDF=${BUILD_DIR}/${object_underscored}_skychart.pdf
+    inkscape -T -A $skychart_PDF ${BUILD_DIR}/${object_underscored}_skychart.svg
 
     # Get DSS image
-    DSS=${dest_dir}/${object_underscored}_dss.png
+    DSS=${BUILD_DIR}/${object_underscored}_dss.png
     DSS_URL=`qdbus org.kde.kstars /KStars org.kde.kstars.getDSSURL "$object"`
     DSS_size_string=`echo $DSS_URL | sed "s/^.*&h=\([0-9\.]*\)&w=\([0-9\.]*\)&.*$/$\2' \\\\\times \1'$/"`
-    wget $DSS_URL -O ${dest_dir}/${object_underscored}_dss.gif
-    convert -negate ${dest_dir}/${object_underscored}_dss.gif $DSS
+    wget $DSS_URL -O ${BUILD_DIR}/${object_underscored}_dss.gif
+    convert -negate ${BUILD_DIR}/${object_underscored}_dss.gif $DSS
 
-    # Set up the LaTeX
-    texfile=${dest_dir}/${object_underscored}.tex
+    # Set up the LaTeX -- TODO: This is bad; we have to find a better way to write this.
+    texfile=${BUILD_DIR}/${object_underscored}.tex
     echo "" > $texfile
     echo "
-\documentclass{article}
-\usepackage{graphicx}
-\usepackage{textcomp}
-\usepackage{gensymb}
-\usepackage{caption}
-\usepackage{subcaption}
-\usepackage{float}
-\setlength{\oddsidemargin}{-0.5in} 
-\setlength{\evensidemargin}{0in}
-\setlength{\marginparwidth}{0in}
-\setlength{\marginparsep}{0in}
-\pagestyle{empty}
-\usepackage[top=0.15in, bottom=0.2in, left=0.5in, right=0in]{geometry}
+\section*{\center \Huge ${Name_Display}}
+\begin{center}
+\Large ${Object_Type} in ${Constellation} \\
+\end{center}
+\vspace{5pt}
 
-\begin{document}
-\title{\Huge ${Name_Display}}
-\author{\Large ${Object_Type} in ${Constellation}}
-\date{}
-\maketitle
-
-\thispagestyle{empty}
-
-%\subsection*{Data}
 \begin{center}
 {\large Data}
 \\\\
@@ -99,10 +163,14 @@ Right Ascension (current) & $ ${RA_HMS} $ & Declination (current) & $ ${Dec_DMS}
 Right Ascension (J2000.0) & $ ${RA_HMS_J2000} $ & Declination (J2000.0) & $ ${Dec_DMS_J2000} $ \\\\
 \hline
 Size & $ ${maj_axis}' \times ${min_axis}' $ & Position Angle & $ ${Position_Angle} \\degree $ \\\\
-Magnitude & $mag & Other Designation & ${Alt_Name} \\\\
+Magnitude & $ ${mag} $ & Other Designation & ${Alt_Name} \\\\
 \hline
 \end{tabular}
 \end{center}
+
+\vspace{5pt}
+${object_description_with_prefix}
+\vspace{5pt}
 
 \begin{figure}[h!]
 \centering
@@ -124,19 +192,43 @@ Magnitude & $mag & Other Designation & ${Alt_Name} \\\\
 
 \begin{figure}[h!]
 \centering
-\includegraphics[width=0.8\textwidth]{Log-form-only.pdf}
+\includegraphics[width=0.8\textwidth]{Logging-Form.pdf}
 \end{figure}
 
 \end{document}
 
 " >> $texfile
 
-cd $dest_dir
-pdflatex -interaction nonstopmode $texfile
+#    cd $BUILD_DIR
+#    pdflatex -interaction nonstopmode $texfile
+#    cd -
+    
+    list="$list ${object_underscored}.pdf"
+    
+    if [[ $constellations != *${Constellation}* ]]; then # Create a list of unique constellations
+	constellations="${constellations} ${Constellation}";
+    fi;
+    
+    if [[ $types != *${Type}* ]]; then # Create a list of unique constellations
+	types="${types} ${Type}";
+    fi;
+    
+    echo "\include{${object_underscored}.tex}" >> ${BUILD_DIR}/Objects.tex
+    
+    echo "${Constellation}|${Type}|${object}|${Name_Display}" >> ${BUILD_DIR}/ConstType.txt
 
-list="$list ${object_underscored}.pdf"
+done <$OBJECT_LIST;
 
+
+# Generate object list by constellation
+for Constellation in $constellations; do
+    \subsection*{${Constellation}} >> ${BUILD_DIR}/ObjectsByConstellation.tex
+    grep "^${Constellation}|" ${BUILD_DIR}/ConstType.txt | awk -F'|' '{ print $NF "\\\\" }' | sort >> ${BUILD_DIR}/ObjectsByConstellation.tex
 done;
 
-pdftk $list cat output Finder-Charts.pdf
+# Generate object list by type
+for Type in $types; do
+    \subsection*{${Type}} >> ${BUILD_DIR}/ObjectsByType.tex
+    grep "^[^|]*|${Type}|" ${BUILD_DIR}/ConstType.txt | awk -F'|' '{ print $NF "\\\\" }' | sort >> ${BUILD_DIR}/ObjectsByType.tex
+done;
 
